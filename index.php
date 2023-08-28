563bde1b7bcfdd05bbce2c6abe7b5a9a4ae0e54b
Hyorine <59877253+Hyorine@users.noreply.github.com>
2023-08-28 19:11:09 +0200

    add business hours

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 function CalculateDueDate($submissionTime, $leadTime) {
+    // Define business hours
+    $businessHoursStart = 9; // 9am
+    $businessHoursEnd = 17; // 5pm
+
     // Convert submission time to a DateTime object
     $submissionDateTime = new DateTime($submissionTime);
+
+    // Check if the submission time is outside business hours
+    if ($submissionDateTime->format('H') < $businessHoursStart) {
+        // If submitted before 9am, set the submission time to 9am
+        $submissionDateTime->setTime($businessHoursStart, 0);
+    } elseif ($submissionDateTime->format('H') >= $businessHoursEnd) {
+        // If submitted at or after 5pm, set the submission time to 9am of the next day
+        $submissionDateTime->add(new DateInterval('P1D')); // Add 1 day
+        $submissionDateTime->setTime($businessHoursStart, 0);
+    }
 
     // Add lead time to submission time
     $submissionDateTime->add(new DateInterval("PT{$leadTime}H")); // Assuming leadTime is in hours
@@ -13,7 +27,7 @@ function CalculateDueDate($submissionTime, $leadTime) {
 }
 
 // Example usage:
-$submissionTime = '2023-08-28 10:00:00'; // Replace with your submission time
+$submissionTime = '2023-08-28 16:00:00'; // Replace with your submission time
 $leadTime = 24; // Replace with your lead time in hours
 
 $dueDate = CalculateDueDate($submissionTime, $leadTime);
